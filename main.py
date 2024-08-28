e75b0fbd4bad0863ddbf8cd686f440c22923d7fc
Ioakeim Mingo <canonshai@gmail.com>
2024-08-28 22:05:49 +0100

    Add CircleShape and Player classes

--- main.py
+++ main.py
@@ -1,20 +1,28 @@
 import pygame
 from constants import *
+from player import Player
 
 
 def main():
     print("Starting asteroids!")
+    # INIT
     pygame.init()
     screen = pygame.display.set_mode(size=(SCREEN_WIDTH, SCREEN_HEIGHT))
     running = True
     clock = pygame.time.Clock()
     dt = 0  # delta time, time passed since last frame
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while running:
-        screen.fill(BACKGROUND_COLOR)
-        pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        screen.fill(BACKGROUND_COLOR)
+        player.draw(screen)
+        pygame.display.flip()
+
+        # used to limit to 60 fps
         # we update our dt, since tick returns ms we divide by 1000 to get secs
         dt = clock.tick(60) / 1000
 
