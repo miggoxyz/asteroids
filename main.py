8e09409581d05dcc33a2dca3fa48321a6eb8f68b
Ioakeim Mingo <canonshai@gmail.com>
2024-08-28 20:52:39 +0100

    Add clock and delta time functionality to game loop

--- main.py
+++ main.py
@@ -7,12 +7,16 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode(size=(SCREEN_WIDTH, SCREEN_HEIGHT))
     running = True
+    clock = pygame.time.Clock()
+    dt = 0  # delta time, time passed since last frame
     while running:
         screen.fill(BACKGROUND_COLOR)
         pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        # we update our dt, since tick returns ms we divide by 1000 to get secs
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
