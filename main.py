1fd566f9621ce43a669393a4b75b4a28643c23ae
Ioakeim Mingo <canonshai@gmail.com>
2024-08-28 20:42:08 +0100

    Create game loop, add background color to constants, initialize pygame, create game window

--- main.py
+++ main.py
@@ -4,8 +4,15 @@ from constants import *
 
 def main():
     print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode(size=(SCREEN_WIDTH, SCREEN_HEIGHT))
+    running = True
+    while running:
+        screen.fill(BACKGROUND_COLOR)
+        pygame.display.flip()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
 
 
 if __name__ == "__main__":
